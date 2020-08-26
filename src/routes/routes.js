238c2ebb92f887cb86c7727ce71a42050a36879e
nigejohnson <nigel.johnson@environment-agency.gov.uk>
2020-08-26 11:42:02 +0100

    module 4

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,17 @@
-import { addnewProduct } from '../controllers/controllers';
+import { addnewProduct, getProducts, getProductWithID } from '../controllers/controllers';
 
 const routes = (app) => {
     app.route('/products')
 
         // Post endpoint
-        .post(addnewProduct);
+        .post(addnewProduct)
+
+        //Get multiple products
+        .get(getProducts);
+
+    app.route('/products/:ProductID')
+        // get a specific product with an ID
+        .get(getProductWithID);
 
 }
 
